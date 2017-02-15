0412b5da42e6f172775cde2af98726c83846e667
kev43 <kev43200@gmail.com>
2017-02-15 15:19:19 +0100

    Creation des fichiers

---
+++ tri-liste.c
@@ -0,0 +1,6 @@
+//
+// Created by kevin on 15/02/17.
+//
+
+#include "tri-liste.h"
+#include <stdlib.h>
---
+++ tri-liste.h
@@ -0,0 +1,11 @@
+//
+// Created by kevin on 15/02/17.
+//
+
+#ifndef TRI_LISTE_TRI_LISTE_H
+#define TRI_LISTE_TRI_LISTE_H
+
+#include "liste.h"
+
+
+#endif //TRI_LISTE_TRI_LISTE_H
